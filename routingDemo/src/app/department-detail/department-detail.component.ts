7d08dae003ecf6a3ffe6b431ff1ea75aefd625ff
Varun <varundgrt@gmail.com>
2020-03-15 18:12:44 +0530

    routing : param map

--- routingDemo/src/app/department-detail/department-detail.component.ts
+++ routingDemo/src/app/department-detail/department-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute,Router,ParamMap} from '@angular/router';
 
 @Component({
   selector: 'app-department-detail',
@@ -7,16 +7,34 @@ import {ActivatedRoute} from '@angular/router';
     <p>
       Seleted department id: {{departmentId}}
     </p>
+    <button (click)="previous()">Previous</button>
+    <button (click)="next()">Next</button>
+
   `,
   styles: []
 })
 export class DepartmentDetailComponent implements OnInit {
   public departmentId;
-  constructor(private route :ActivatedRoute) { }
+  constructor(private route :ActivatedRoute,private newRoute:Router) { }
 
   ngOnInit(): void {
-    let id=this.route.snapshot.paramMap.get('id');
-    this.departmentId= parseInt(id);
+    //Static routing  in this angular doenst call the method and repaint the view
+    // let id=this.route.snapshot.paramMap.get('id');
+    // this.departmentId= parseInt(id);
+
+    //dynamic
+    this.route.paramMap.subscribe((param : ParamMap)=>{
+      this.departmentId = parseInt(param.get('id'));
+    })
+  }
+
+  previous(){
+    let previousId = this.departmentId-1;
+    this.newRoute.navigate(['/departments',previousId]);
+  }
+  next(){
+    let nextId = this.departmentId+1;
+    this.newRoute.navigate(['/departments',nextId]);
   }
 
 }
